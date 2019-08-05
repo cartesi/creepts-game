9c4d321a20d8b3cd7602d5a3632833bc05e8f740
David Escriche Oro <davidescriche6@gmail.com>
2019-08-05 17:48:51 +0200

    new assets

--- src/scenes/battle-scene/turret-actors/BulletActor.ts
+++ src/scenes/battle-scene/turret-actors/BulletActor.ts
@@ -3,15 +3,21 @@ import { GameConstants } from "../../../GameConstants";
 export class BulletActor extends Phaser.GameObjects.Image {
 
     public anutoBullet: Anuto.Bullet;
+    public initialPosition: {x: number, y: number};
 
     constructor(scene: Phaser.Scene, anutoBullet: Anuto.Bullet) {
 
-        super(scene, 0, 0, "texture_atlas_1", "bullet");
+        super(scene, 0, 0, "texture_atlas_1", "bullet_1_1");
 
         this.anutoBullet = anutoBullet;
 
+        this.setScale(.75);
+        this.visible = false;
+
         this.x = this.anutoBullet.x * GameConstants.CELLS_SIZE;
         this.y = this.anutoBullet.y * GameConstants.CELLS_SIZE;
+
+        this.initialPosition = {x: this.x, y: this.y};
     }
 
     public update(time: number, delta: number): void {
@@ -23,8 +29,21 @@ export class BulletActor extends Phaser.GameObjects.Image {
         } else {
             smoothFactor = 1;
         }
+
+        let offX = (this.anutoBullet.x * GameConstants.CELLS_SIZE - this.x) * smoothFactor;
+        let offY = (this.anutoBullet.y * GameConstants.CELLS_SIZE - this.y) * smoothFactor;
         
-        this.x += (this.anutoBullet.x * GameConstants.CELLS_SIZE - this.x) * smoothFactor;
-        this.y += (this.anutoBullet.y * GameConstants.CELLS_SIZE - this.y) * smoothFactor;
+        this.x += offX;
+        this.y += offY;
+
+        this.rotation = Math.atan2(offY, offX) + Math.PI / 2;
+
+        if (!this.visible) {
+            let distX = this.initialPosition.x - this.x;
+            let distY = this.initialPosition.y - this.y;
+            if (Math.sqrt( distX * distX + distY * distY) > 40) {
+                this.visible = true;
+            }
+        }
     }
 }
