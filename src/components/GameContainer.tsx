476e2b86b55b24be7a3aedc92b75e93107983743
Danilo Tuler <danilo@cartesi.io>
2019-09-15 00:29:54 -0400

    Simplifies game position and resizing

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -14,24 +14,13 @@ export default class GameContainer extends React.Component<IGameContainerProps, 
 
     constructor(props: IGameContainerProps) {
         super(props);
-        this.state = {
-            height: GameConstants.GAME_HEIGHT
-        }
     }
 
     componentDidMount() {
         this.createGame();
-        window.addEventListener("resize", this.updateDimensions.bind(this));
     }
 
     componentWillUnmount() {
-        window.removeEventListener("resize", this.updateDimensions.bind(this));
-    }
-
-    updateDimensions() {
-        // use whole window height as game height
-        // width is auto adjusted to keep aspect ratio
-        this.setState({ height: window.innerHeight });
     }
 
     createGame() {
@@ -44,7 +33,8 @@ export default class GameContainer extends React.Component<IGameContainerProps, 
             width: GameConstants.GAME_WIDTH,
             height: GameConstants.GAME_HEIGHT,
             scale: {
-                mode: Phaser.Scale.FIT
+                mode: Phaser.Scale.FIT,
+                autocenter: Phaser.Scale.CENTER_BOTH
             },
 
             scene: [
@@ -57,24 +47,21 @@ export default class GameContainer extends React.Component<IGameContainerProps, 
 
         // If compilation error here, compare Phaser definitions file of working copy (phaser.d.ts, line 48040 on 27-05-2019)
         // Also make sure to delete all *.ts files in node_modules/trailz folder
-        const game = new Game(gameConfig);
-        game.events
-            .on(Phaser.Core.Events.BOOT, () => { console.log("BOOT"); })
-            .on(Phaser.Core.Events.CONTEXT_LOST, () => { console.log("CONTEXT_LOST"); })
-            .on(Phaser.Core.Events.CONTEXT_RESTORED, () => { console.log("CONTEXT_RESTORED"); })
-            .on(Phaser.Core.Events.DESTROY, () => { console.log("DESTROY"); })
-            .on(Phaser.Core.Events.FOCUS, () => { console.log("FOCUS"); })
-            .on(Phaser.Core.Events.HIDDEN, () => { console.log("HIDDEN"); })
-            .on(Phaser.Core.Events.PAUSE, () => { console.log("PAUSE"); })
-            .on(Phaser.Core.Events.READY, () => { console.log("READY"); })
-            .on(Phaser.Core.Events.RESUME, () => { console.log("RESUME"); })
-            .on(Phaser.Core.Events.VISIBLE, () => { console.log("VISIBLE"); });
-
-        this.updateDimensions();
+        new Game(gameConfig);
     }
 
     public render() {
         const visibility = this.props.visible ? "visible" : "hidden";
-        return <div id="phaser-game" style={{ height: `${this.state.height}px`, visibility }} />;
+        return <div
+            id="phaser-game"
+            style={{
+                height: "100vh",
+                width: "100vw",
+                position: "absolute",
+                left: 0,
+                top: 0,
+                visibility
+            }} 
+        />;
     }
 }
