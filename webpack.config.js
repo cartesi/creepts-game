f935f856db553ee97bb38d7640e8ea09e68ecec5
Danilo Tuler <danilo@cartesi.io>
2019-09-17 19:06:57 -0400

    Running dev server at 0.0.0.0, so we can test on mobile

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
 	devServer: {
 		contentBase: path.resolve(__dirname, './'),
 		publicPath: '/dist/',
-		host: '127.0.0.1',
+		host: '0.0.0.0',
 		port: 8080,
 		open: true,
 		historyApiFallback: true
