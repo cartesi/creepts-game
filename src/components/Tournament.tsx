15732364ee68f732f06682ae2bb5cbc7b8773828
Danilo Tuler <danilo@cartesi.io>
2019-09-17 16:35:29 -0400

    Reacts to exit menu option

--- src/components/Tournament.tsx
+++ src/components/Tournament.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { GameManager } from "../GameManager";
+import { navigate } from "hookrouter";
 
 interface IProps { id: string }
 interface IState { }
@@ -15,11 +16,18 @@ export default class Tournament extends Component<IProps, IState> {
         "civyshk_2y",
         "civyshk_line5",
         "civyshk_labyrinth",
-    ]
+    ];
 
     componentDidMount() {
         const map = this.props.id;
+
+        // select the map of the tournament
         GameManager.mapSelected(this.maps.indexOf(map));
+
+        // exit button return no home
+        GameManager.events.on("exit", () => {
+            navigate('/');
+        });
     }
 
     render() {
