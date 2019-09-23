92a7abbbf84435adeb0a3801fb2a0a272c5ed68e
Danilo Tuler <danilo@cartesi.io>
2019-09-23 13:18:33 -0400

    Using copy-webpack-plugin and html-webpack-plugin
    Static build of the game. Uses copy-webpack-plugin to copy assets to dist and html-webpack-plugin to dynamically generate index.html into dist.
    
    Dockerfile to run the game as an nginx container.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const pathToPhaser = path.join(__dirname, '/node_modules/phaser/');
 const phaser = path.join(pathToPhaser, 'dist/phaser-arcade-physics.min');
@@ -8,7 +10,7 @@ module.exports = env => ({
 	devtool: 'source-map',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
+		filename: '[name]-bundle.js',
 	},
 	module: {
 		rules: [
@@ -17,14 +19,19 @@ module.exports = env => ({
 		]
 	},
 	devServer: {
-		contentBase: path.resolve(__dirname, './'),
-		publicPath: '/dist/',
+		contentBase: path.resolve(__dirname, 'dist'),
 		host: '0.0.0.0',
 		port: 8080,
-		open: true,
+		compress: true,
 		historyApiFallback: true
 	},
 	plugins: [
+		new CopyWebpackPlugin([
+			{ from: 'assets/', to: 'assets/' }
+		]),
+		new HtmlWebpackPlugin({
+			template: 'index.html'
+		}),
 		new webpack.DefinePlugin({
 			__GAME_ONLY__: JSON.stringify(env && env.GAME_ONLY || false)
 		})
