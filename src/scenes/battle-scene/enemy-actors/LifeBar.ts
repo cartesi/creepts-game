49426aa83826e1acb6919be979fb962949358528
David Escriche Oro <davidescriche6@gmail.com>
2019-11-18 10:38:47 +0100

    Show life bar on first hit

--- src/scenes/battle-scene/enemy-actors/LifeBar.ts
+++ src/scenes/battle-scene/enemy-actors/LifeBar.ts
@@ -2,9 +2,9 @@ import { GameVars } from "../../../GameVars";
 
 export class LifeBar extends Phaser.GameObjects.Container {
 
-    public static readonly WIDTH = 40;
+    public static readonly WIDTH = 43;
 
-    private bar: Phaser.GameObjects.Graphics;
+    private bar: Phaser.GameObjects.Image;
     private totalLife: number;
 
     constructor(scene: Phaser.Scene, totalLife: number) {
@@ -18,26 +18,18 @@ export class LifeBar extends Phaser.GameObjects.Container {
         background.fillRect(0, 0, 40, 4);
         this.add(background);
 
-        this.bar = new Phaser.GameObjects.Graphics(this.scene);
-        this.bar.fillStyle(0x00FF00);
-        this.bar.fillRect(0, 0, 40, 4);
+        this.bar = new Phaser.GameObjects.Image(this.scene, 0, 0, "texture_atlas_1", "life_bar_1");
+        this.bar.setOrigin(0);
         this.add(this.bar);
+
+        this.visible = false;
     }
 
     public updateValue(life: number): void {
 
-        // if ((life / this.totalLife) > this.bar.scaleX) {
-
-        //     this.scene.tweens.add({
-        //         targets: this.bar,
-        //         alpha: 0,
-        //         ease: Phaser.Math.Easing.Linear.Linear,
-        //         duration: GameVars.timeStepFactor === 4 ? 20 : 60,
-        //         yoyo: true,
-        //         repeat: 3
-        //     });
-        // }
+        let num = 41 - Math.round((life / this.totalLife) * 40);
+        this.bar.setFrame("life_bar_" + Math.min(num, 40));
         
-        this.bar.scaleX = life / this.totalLife;
+        // this.bar.scaleX = life / this.totalLife;
     }
 }
